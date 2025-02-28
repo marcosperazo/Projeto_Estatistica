0da52671df2b12f405b38a68cb6c052f410b287c
Marcos Perazo <marcosperazo@icloud.com>
2025-02-27 22:32:05 -0300

    Inclusão do ggplot2

--- test.R
+++ test.R
@@ -6,20 +6,52 @@ install.packages('datatable')
 install.packages('readxl')
 install.packages('rvest')
 install.packages('tinytex')
+install.packages("ggplot2")
 tinytex::install_tinytex(force = TRUE)
 tinytex::install_tinytex()
+
 
 library(tinytex)
 library(tidyverse)
 library(rvest)
 library(data.table)
 library(readxl)
+library(dplyr)
+library(ggplot2)
+
 renv::snapshot()
 renv::restore()
 
 
 # Evolução da frota de veículos por unidade da federação e tipo de veículo
 frota <- readr::read_csv("br_denatran_frota_uf_tipo.csv")
+frota
+kable(head(frota))
+
+# Frota do estado de São Paulo
+frota_sp <- frota %>%
+  filter(mes == 12, sigla_uf == "SP") %>%
+  select(-mes, -sigla_uf)
+
+
+frota_sp
+
+# Filtrar os dados para incluir apenas 'automovel'
+nova_frota_automovel <- frota_sp %>%
+  filter(tipo_veiculo == "automovel")
+
+# Criar o gráfico
+ggplot(data = nova_frota_automovel, aes(x = ano, y = quantidade)) +
+  geom_line() +
+  labs(title = "Quantidade Total de Automóveis por Ano",
+       x = "Ano",
+       y = "Quantidade Total") +
+  theme_minimal()
+
+
+
+
+
 
 # Filtrando os automoveis
 automoveis <- frota %>%
